4db3dfe80baaea0d02204487af94c3e1444fd0fb
Tiziano Perrucci <tiziano.perrucci@gmail.com>
2013-10-04 11:54:57 +0200

    at boot changes working directory to prudentia dir

--- prudentia.py
+++ prudentia.py
@@ -1,10 +1,6 @@
-import os
 import sys
 import logging
 from src.cli import CLI
-
-# Change cwd in prudentia dir
-os.chdir(os.path.dirname(os.path.realpath(__file__)))
 
 logging.basicConfig(format='%(asctime)s.%(msecs).03d [%(name)s] %(levelname)s: %(message)s',
     datefmt='%d-%m-%Y %H:%M:%S', level=logging.INFO)
