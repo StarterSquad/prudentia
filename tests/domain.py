f47daa1f9f2f722fe67cd49ae281c8cd1c3b5e14
Tiziano Perrucci <tiziano.perrucci@gmail.com>
2013-12-02 11:31:38 +0100

    fixes test

--- tests/domain.py
+++ tests/domain.py
@@ -5,16 +5,30 @@ from src.domain import Environment, Box
 class TestEnvironment(unittest.TestCase):
 
     def setUp(self):
-        self.env = Environment('./env')
-        self.testBox = Box('name', 'dev.yml', '0.0.0.0')
+        self.env = Environment('./env', NoExtra)
+        box = Box()
+        box.set_name('box-name')
+        box.set_playbook('dev.yml')
+        box.set_ip('0.0.0.0')
+        box.set_extra(NoExtra())
+        self.testBox = box
 
     def test_add(self):
         self.env.add(self.testBox)
         self.assertEqual(json.load(open('./env/.boxes', 'r'))[0]['name'], self.testBox.name)
 
     def test_remove(self):
-        self.env.remove(self.testBox)
+        self.env.remove('box-name')
         self.assertEqual(json.load(open('./env/.boxes', 'r')), [])
+
+class NoExtra:
+    def toJson(self):
+        return ''
+
+    @staticmethod
+    def fromJson(json):
+        return NoExtra()
+
 
 if __name__ == '__main__':
     unittest.main()
