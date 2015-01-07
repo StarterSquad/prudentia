782acc90d9bc52f203707a9f5b8bae7228e7742c
Tiziano Perrucci <tiziano.perrucci@gmail.com>
2015-01-07 01:38:45 +0100

    Clean

--- prudentia.sh
+++ prudentia.sh
@@ -1,32 +1,11 @@
 #!/bin/bash
 
-#http://stackoverflow.com/questions/59895/can-a-bash-script-tell-what-directory-its-stored-in
-SOURCE="${BASH_SOURCE[0]}"
-# resolve $SOURCE until the file is no longer a symlink
-while [ -h "${SOURCE}" ]; do
-  DIR="$( cd -P "$( dirname "${SOURCE}" )" && pwd )"
-  SOURCE="$(readlink "${SOURCE}")"
-  # if $SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located
-  [[ ${SOURCE} != /* ]] && SOURCE="${DIR}/${SOURCE}"
-done
-DIR="$( cd -P "$( dirname "${SOURCE}" )" && pwd )"
-
-# Change cwd in Prudentia dir
-cd ${DIR}
-
 if [ -z "$( which python )" ]; then
     echo "Please, install Python (>=2.6)."
     exit 1
 elif [ -z "$( which virtualenv )" ]; then
     echo "Please, install Virtualenv."
     exit 1
-fi
-
-# If Prudentia runs from the develop branch pulls from origin if the local and remote commit aren't the same
-if [ "$( /usr/bin/git rev-parse --abbrev-ref HEAD )" == "develop" ]; then
-  if [ ! "$( /usr/bin/git log --pretty=%H ...refs/heads/develop^ )" == "$( /usr/bin/git ls-remote origin -h refs/heads/develop |cut -f1 )" ]; then
-    /usr/bin/git pull
-  fi
 fi
 
 if [ ! -d "./p-env/" ]; then
