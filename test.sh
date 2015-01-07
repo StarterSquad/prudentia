6c7df70d2eb5a1d4020150bbb7d8e874b0e8ed52
Tiziano Perrucci <tiziano.perrucci@gmail.com>
2015-01-07 04:01:21 +0100

    Merge test scripts

--- test.sh
+++ test.sh
@@ -1,3 +1,25 @@
 #!/bin/bash
 
+python -c 'import sys; print sys.real_prefix' 2>/dev/null && INVENV=1 || INVENV=0
+
+if [ ${INVENV} == 0 ]; then
+  echo -e "No active Virtual Environment.\n"
+  if [ ! -d "./p-env/" ]; then
+    virtualenv p-env
+  fi
+
+  source ./p-env/bin/activate
+
+  TMP_DEPS=/tmp/prudentia_test_temp_deps_${RANDOM}
+  pip freeze -l > ${TMP_DEPS}
+  if ! cmp ./requirements.txt ${TMP_DEPS} > /dev/null 2>&1
+  then
+    echo "Installing Python dependencies ..."
+    cat ${TMP_DEPS}
+    pip install -r ./requirements.txt
+  fi
+else
+  echo -e "Virtual Env active.\n"
+fi
+
 nosetests -c nose.cfg $@
