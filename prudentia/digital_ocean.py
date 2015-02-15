11f6d31c551c66773fa716fc6d62873eeaf1f402
Tiziano Perrucci <tiziano.perrucci@gmail.com>
2015-02-15 18:25:02 +0100

    Upgrades Digital Ocean default Ubuntu image

--- prudentia/digital_ocean.py
+++ prudentia/digital_ocean.py
@@ -21,7 +21,7 @@ class DigitalOceanCli(FactoryCli):
 class DigitalOceanProvider(FactoryProvider):
     NAME = 'digital-ocean'
 
-    DEFAULT_IMAGE_ID = 6918990  # Ubuntu 14.04 x64
+    DEFAULT_IMAGE_ID = 9801950  # Ubuntu 14.04 x64
     DEFAULT_SIZE_ID = 63  # 1GB
     DEFAULT_REGION_ID = 5  # Amsterdam 2
 
