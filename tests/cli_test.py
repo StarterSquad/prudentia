7e2d25a35e28d1e79543649c0c4a4f7bea61bd2c
Tiziano Perrucci <tiziano.perrucci@gmail.com>
2015-07-19 16:45:21 +0200

    improve(cli): Refactors main cli to properly parse input arguments improve(cli): Accepts list of commands as arguments (before could run only one) feature(version): Introduce -v (--version) argument to print current Prudentia version improve(test): Simplifies main cli tests

--- tests/cli_test.py
+++ tests/cli_test.py
@@ -7,30 +7,11 @@ class TestCli(unittest.TestCase):
     def setUp(self):
         self.cli = CLI()
 
+    def _parse_run(self, *args):
+        return self.cli.run(self.cli.parse(args))
 
-    # CmdLoop tests return always False indicating that the execution will not be terminated
-    def test_loop_use_wrong_env(self):
-        self.cli.loop_initiated = True
-        self.assertEqual(self.cli.do_use('whatever-env'), False)
+    def test_run_failing_provision(self):
+        self.assertFalse(self._parse_run('local', 'provision whatever-box'))
 
-    def test_loop_run_failing_provision(self):
-        self.cli.loop_initiated = True
-        self.assertEqual(self.cli.do_use('local', 'provision', 'whatever-box'), False)
-
-    def test_loop_run_list(self):
-        self.cli.loop_initiated = True
-        self.assertEqual(self.cli.do_use('local', 'list'), False)
-
-
-    # OneCmd tests will return the value of provider.provisioned
-    def test_onecmd_use_wrong_env(self):
-        self.cli.loop_initiated = False
-        self.assertEqual(self.cli.do_use('whatever-env'), False)
-
-    def test_onecmd_run_failing_provision(self):
-        self.cli.loop_initiated = False
-        self.assertEqual(self.cli.do_use('local', 'provision', 'whatever-box'), False)
-
-    def test_onecmd_run_list(self):
-        self.cli.loop_initiated = False
-        self.assertEqual(self.cli.do_use('local', 'list'), False)
+    def test_run_list(self):
+        self.assertFalse(self._parse_run('local', 'list'))
