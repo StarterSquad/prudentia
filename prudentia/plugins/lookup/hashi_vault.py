c83bfa9ce46095361240ae756c5edb72cf10c58f
Vyacheslav Voronenko <git@voronenko.info>
2015-09-16 10:17:26 +0300

    improve(hashi_vault) Fixed security error for ansible 1.9 by changing parameter separator.

--- prudentia/plugins/lookup/hashi_vault.py
+++ prudentia/plugins/lookup/hashi_vault.py
@@ -39,6 +39,7 @@ ANSIBLE_HASHI_VAULT_ADDR = 'http://127.0.0.1:8200'
 if os.getenv('VAULT_ADDR') is not None:
     ANSIBLE_HASHI_VAULT_ADDR = os.environ['VAULT_ADDR']
 
+
 class HashiVault:
     def __init__(self, **kwargs):
         try:
@@ -74,19 +75,16 @@ class LookupModule(object):
 
     def run(self, terms, inject=None, **kwargs):
 
-        terms = safe_eval(terms)
+#        terms = safe_eval(terms)
         vault_args = terms.split(' ')
         vault_dict = {}
         ret = []
 
         for param in vault_args:
-            key, value = param.split('=')
+            key, value = param.split('|')
             vault_dict[key] = value
 
         vault_conn = HashiVault(**vault_dict)
-
-        for term in terms:
-            key = term.split()[0]
-            value = vault_conn.get()
-            ret.append(value)
+        value = vault_conn.get()
+        ret.append(value)
         return ret
