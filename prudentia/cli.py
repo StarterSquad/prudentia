78068b4201b915fcd5c7ce92a9f7d2a17e000591
Vyacheslav Voronenko <git@voronenko.info>
2015-09-16 21:08:21 +0300

    improve(vault) implemented hashi_vault_template module for clear syntax for vault binary files.

--- prudentia/cli.py
+++ prudentia/cli.py
@@ -11,6 +11,7 @@ cwd = path.dirname(path.realpath(__file__))
 os.environ['ANSIBLE_CONFIG'] = path.join(cwd, 'ansible.cfg')
 os.environ['ANSIBLE_ROLES_PATH'] = path.join(cwd, 'roles') + os.pathsep + '/etc/ansible/roles'
 os.environ['ANSIBLE_LOOKUP_PLUGINS'] = path.join(cwd, 'plugins', 'lookup') + os.pathsep + '/usr/share/ansible_plugins/lookup_plugins'
+os.environ['ANSIBLE_LIBRARY'] = path.join(cwd, 'modules')
 
 from digital_ocean import DigitalOceanCli
 from local import LocalCli
