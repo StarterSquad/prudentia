81eb2791bbef719e06d316d12458a16e51d1d89e
Vyacheslav Voronenko <git@voronenko.info>
2016-07-21 17:09:51 +0300

    refactor(hashi_vault) made plugin compatible with Ansible 2, tested with ansible 2.0.2.0

--- prudentia/plugins/lookup/hashi_vault.py
+++ prudentia/plugins/lookup/hashi_vault.py
@@ -1,54 +1,44 @@
-# (c) 2015, Jonathan Davila <jdavila(at)ansible.com>
-#
-# This file is part of Ansible
-#
-# Ansible is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-#
-# Ansible is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with Ansible.  If not, see <http://www.gnu.org/licenses/>.
-#
-# USAGE: {{ lookup('hashi_vault', 'secret=secret/hello token=c975b780-d1be-8016-866b-01d0f9b688a5 url=http://myvault:8200')}}
-#
-# You can skip setting the url if you set the VAULT_ADDR environment variable
-# or if you want it to default to localhost:8200
-#
-# NOTE: Due to a current limitation in the HVAC library there won't
-# necessarily be an error if a bad endpoint is specified.
-#
-# Requires hvac library. Install with pip.
-#
-
 from __future__ import (absolute_import, division, print_function)
-__metaclass__ = type
-
 import os
+from ansible.errors import AnsibleError
+from ansible.plugins.lookup import LookupBase
 
-from ansible.errors import *
+__metaclass__ = type
 
 ANSIBLE_HASHI_VAULT_ADDR = 'http://127.0.0.1:8200'
 
 if os.getenv('VAULT_ADDR') is not None:
     ANSIBLE_HASHI_VAULT_ADDR = os.environ['VAULT_ADDR']
 
+if os.getenv('VAULT_TOKEN') is not None:
+    ANSIBLE_HASHI_VAULT_TOKEN = os.environ['VAULT_TOKEN']
 
-class HashiVaultLookup:
+
+class SaHashiVault:
     def __init__(self, **kwargs):
         try:
             import hvac
         except ImportError:
             AnsibleError("Please pip install hvac to use this module")
 
-        self.url = kwargs.pop('url')
-        self.secret = kwargs.pop('secret')
-        self.token = kwargs.pop('token')
+        self.url = kwargs.get('url', ANSIBLE_HASHI_VAULT_ADDR)
+
+        self.token = kwargs.get('token', ANSIBLE_HASHI_VAULT_TOKEN)
+        if self.token is None:
+            raise AnsibleError("No Vault Token specified")
+
+        # split secret arg, which has format 'secret/hello:value'
+        # into secret='secret/hello' and secret_field='value'
+        s = kwargs.get('secret')
+        if s is None:
+            raise AnsibleError("No secret specified")
+
+        s_f = s.split(':')
+        self.secret = s_f[0]
+        if len(s_f) >= 2:
+            self.secret_field = s_f[1]
+        else:
+            self.secret_field = 'value'
 
         self.client = hvac.Client(url=self.url, token=self.token)
 
@@ -60,27 +50,39 @@ class HashiVaultLookup:
     def get(self):
         data = self.client.read(self.secret)
         if data is None:
-            raise AnsibleError("The secret %s doesn't seem to exist" % self.secret)
-        else:
-            return data['data']['value']
+            raise AnsibleError("The secret %s doesn't seem to exist"
+                               % self.secret)
+
+        if self.secret_field == '':  # secret was specified with trailing ':'
+            return data['data']
+
+        if self.secret_field not in data['data']:
+            raise AnsibleError("The secret %s does not contain the field '%s'."
+                               % (self.secret, self.secret_field))
+
+        return data['data'][self.secret_field]
 
 
-class LookupModule(object):
-    def __init__(self, basedir=None, **kwargs):
-        pass
-
-    def run(self, terms, inject=None, **kwargs):
-        vault_args = terms.split(' ')
+class LookupModule(LookupBase):
+    def run(self, terms, variables, **kwargs):
+        vault_args = terms[0].split(' ')
         vault_dict = {}
         ret = []
 
         for param in vault_args:
-            key, value = param.split('|')
+            try:
+                key, value = param.split('=')
+            except ValueError as e:
+                raise AnsibleError("hashi_vault plugin needs key=value pairs, but received %s %s"
+                                   % (terms, e.message))
             vault_dict[key] = value
 
-        vault_conn = HashiVaultLookup(**vault_dict)
-        value = vault_conn.get()
-        ret.append(value)
+        vault_conn = SaHashiVault(**vault_dict)
+
+        for term in terms:
+            key = term.split()[0]
+            value = vault_conn.get()
+            ret.append(value)
 
         if 'write_to_file' in vault_dict.keys():
             text_file = open(vault_dict['write_to_file'], "w")
